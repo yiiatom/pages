c5498ea110b69462d310dc1399f0d7e5dc8c7700
Dmitry Khlystov <dkhlystov@gmail.com>
2026-08-20 13:24:11 +1000

    Add translation for sidebar

--- src/Listener/SidebarMenuListener.php
+++ src/Listener/SidebarMenuListener.php
@@ -7,16 +7,20 @@ namespace Atom\Pages\Listener;
 use Atom\Entity\UserRole;
 use Atom\Sidebar\Event\SidebarMenuEvent;
 use Atom\Sidebar\SidebarMenuItem;
+use Yiisoft\Translator\TranslatorInterface;
 
 final class SidebarMenuListener
 {
     public function __construct(
+        private TranslatorInterface $translator,
     ) {}
 
     public function __invoke(SidebarMenuEvent $event): void
     {
+        $t = $this->translator->withDefaultCategory('atom-pages');
+
         $event->addItem(new SidebarMenuItem(
-            label: 'Pages',
+            label: $t->translate('Pages'),
             routeName: 'atom.page.index',
             icon: 'fa-solid fa-file-lines',
             requiredRole: UserRole::ADMIN,
